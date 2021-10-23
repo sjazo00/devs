5b4e410d27758c047c853470cf57e5abebe05c8f
SandroJažo <sjazo00@fesb.hr>
2021-10-23 18:53:54 +0200

    Dashboard Part 2

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,9 @@ import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
 import { clearCurrentProfile } from './actions/profileActions';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
 
 // Check for token
 if(localStorage.jwtToken) {
@@ -54,6 +57,15 @@ function App() {
             </Switch>
             <Switch>
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/add-experience" component={AddExperience} />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/add-education" component={AddEducation} />
             </Switch>
           </div>
           <Footer />
